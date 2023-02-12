4d52f6ca79aa27450dddd26e929c4d48e615d7d1
Devon Carew <devoncarew@gmail.com>
2023-02-11 18:51:53 -0800

    more changes (#3)
    * more changes
    
    * fix default branch name
    
    * fix sdk ref
    
    * fix matrix reference
    
    * update names

--- pkgs/shelf_proxy/lib/shelf_proxy.dart
+++ pkgs/shelf_proxy/lib/shelf_proxy.dart
@@ -101,5 +101,6 @@ Handler proxyHandler(Object url, {http.Client? client, String? proxyName}) {
 /// gracefully.
 void _addHeader(Map<String, String> headers, String name, String value) {
   final existing = headers[name];
+
   headers[name] = existing == null ? value : '$existing, $value';
 }
